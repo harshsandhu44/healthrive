a5f9a8cb54487eb339310ac8221b20b56093a8ff
Harsh Sandhu <hsandhu@speareducation.com>
2025-08-03 22:53:52 +0530

    feat: create sidebar

---
+++ src/components/sidebars/app-sidebar.tsx
@@ -0,0 +1,38 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export const AppSidebar = ({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) => {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <Link href="#">
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent></SidebarContent>
+      <SidebarFooter>
+        <UserButton />
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
