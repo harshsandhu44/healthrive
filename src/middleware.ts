46f358d64cb80517376d86970310f2173816e7e1
Harsh Sandhu <hsandhu@speareducation.com>
2025-08-03 22:40:00 +0530

    feat: navigate user to /organization if user is not part of an org

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
+  const { orgId } = await auth();
+
   if (isPublicRoute(req)) {
     return;
+  }
+
+  if (!orgId && !req.url.includes("/organization")) {
+    return NextResponse.redirect(new URL("/organization", req.url));
   }
 
   await auth.protect();
