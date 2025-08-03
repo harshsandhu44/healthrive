58b37f52cc915d5a656f7e7ca79ffd41cfa08ac2
Harsh Sandhu <hsandhu@speareducation.com>
2025-08-03 18:21:24 +0530

    feat: setup auth pages

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function PublicLayout({ children }: PropsWithChildren) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
