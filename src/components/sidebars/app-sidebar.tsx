3e0f2090f8860728425e19f3595f440d9493326a
Harsh Sandhu <hsandhu@speareducation.com>
2025-08-04 06:34:57 +0530

    feat: setup sidebar menu items

--- src/components/sidebars/app-sidebar.tsx
+++ src/components/sidebars/app-sidebar.tsx
@@ -2,17 +2,27 @@ import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { LayoutDashboardIcon, PhoneCallIcon } from "lucide-react";
+import { Logo } from "../icons";
 
 export const AppSidebar = ({
   ...props
 }: React.ComponentProps<typeof Sidebar>) => {
+  const navItems = [
+    { icon: LayoutDashboardIcon, label: "Dashboard", href: "/dashboard" },
+    { icon: PhoneCallIcon, label: "Appointments", href: "/appointments" },
+  ];
+
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
@@ -22,16 +32,53 @@ export const AppSidebar = ({
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <Link href="#">
-                <span className="text-base font-semibold">Acme Inc.</span>
+              <Link href="/dashboard">
+                <Logo className="size-8" />
+                <span className="text-base font-semibold">Healthrive</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
-      <SidebarContent></SidebarContent>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navItems.map((item) => (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton
+                    asChild
+                    className="data-[slot=sidebar-menu-button]:!p-1.5"
+                  >
+                    <Button
+                      asChild
+                      className="justify-start w-full"
+                      variant="ghost"
+                    >
+                      <Link href={item.href}>
+                        <item.icon />
+                        {item.label}
+                      </Link>
+                    </Button>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
       <SidebarFooter>
-        <UserButton />
+        <UserButton
+          showName
+          appearance={{
+            elements: {
+              rootBox: "w-full",
+              userButtonTrigger: "w-full justify-start",
+              userButtonBox: "flex-row-reverse",
+              userButtonOuterIdentifier: "font-medium",
+            },
+          }}
+        />
       </SidebarFooter>
     </Sidebar>
   );
