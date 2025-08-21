53f68680dc1e1a58836875a1290c7d172fb7d5b5
Harsh Sandhu <hsandhu@speareducation.com>
2025-08-22 00:14:04 +0530

    chore: create protected routes

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default async function SignInPage() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
---
+++ src/app/(protected)/patients/page.tsx
@@ -0,0 +1,8 @@
+export default function PatientsPage() {
+  return (
+    <div>
+      <h1>Patients</h1>
+      <p>Manage your patients here!</p>
+    </div>
+  );
+}
