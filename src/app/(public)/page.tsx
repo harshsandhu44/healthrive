d333a3c538381d8639616d100013f62b1f121896
Harsh Sandhu <hsandhu@speareducation.com>
2025-08-22 12:53:31 +0530

    feat: setup fonts

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Welcome to Vylune!</h1>
-      <p>This is a sample page.</p>
-    </div>
+    <main>
+      <section className="h-screen container flex items-center justify-center font-mono">
+        <div className="space-y-1">
+          <h1 className="text-5xl md:text-7xl xl:text-9xl">
+            Welcome to Vylune
+          </h1>
+          <p className="text-xl text-pretty">
+            On the mission to build a 21st Century Healthcare System
+          </p>
+          <Button size="lg" className="mt-4 w-full md:w-fit text-lg">
+            Let&apos;s get started!
+          </Button>
+        </div>
+      </section>
+    </main>
   );
 }
